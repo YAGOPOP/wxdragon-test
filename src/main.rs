60a29edf58307e468efe49adf5ed51b80f4c9a35
YAGOPOP <ttatarinov50@gmail.com>
2026-06-26 23:00:49 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use wxdragon::appearance::*;
+use wxdragon::prelude::*;
+
+fn main() {
+    let _ = wxdragon::main(|_| {
+        let frame = Frame::builder()
+            .with_title("Hello, World!")
+            .with_size(Size::new(300, 200))
+            .build();
+
+        if let Some(app) = wxdragon::app::get_app() {
+            app.set_appearance(Appearance::Light);
+            dbg!(Appearance::System);
+        }
+
+        let sizer = BoxSizer::builder(Orientation::Vertical).build();
+
+        let button = Button::builder(&frame).with_label("Click me").build();
+
+        button.on_click(|_| {
+            println!("Button clicked");
+        });
+
+        sizer.add(
+            &button,
+            0,
+            SizerFlag::AlignCenterHorizontal | SizerFlag::AlignCenterVertical,
+            0,
+        );
+
+        frame.set_sizer(sizer, true);
+        frame.show(true);
+        frame.centre();
+    });
+}
